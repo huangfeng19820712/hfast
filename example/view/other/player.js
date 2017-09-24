149b21420a3beb48a43db2db7445c6936442362d
longhumen <huangfeng19820712@gmail.com>
2017-09-25 07:00:40 +0800

    修改播放器用例

--- example/view/other/player.js
+++ example/view/other/player.js
@@ -22,44 +22,9 @@ define(["core/js/layout/FluidLayout",
                             mainRegion: {
                                 comXtype: $Component.PLAYER,
                                 height: "600px",
-                                width: "400px",
                                 comConf: {
                                     playerMode: $cons.playerMode.FLV,
-                                    url: "http://10683.liveplay.myqcloud.com/live/10683_475805.flv"
-                                }
-                            }
-                        }
-                    },{
-                        comXtype: $Component.PANEL,
-                        comConf: {
-                            title: "播放器",
-                            theme: $Theme.BLUE,
-                            help: "播放器",
-                            iconSkin: ViewUtils.createIconClass(CommonConstant.Icon.EDIT),
-                            mainRegion: {
-                                comXtype: $Component.PLAYER,
-                                height: "600px",
-                                width: "400px",
-                                comConf: {
-                                    playerMode: $cons.playerMode.RTMP,
-                                    url: "rtmp://rtmp.new-live.net/yuemei/14426_2bccf18f64d5eed07236"
-                                }
-                            }
-                        }
-                    },{
-                        comXtype: $Component.PANEL,
-                        comConf: {
-                            title: "播放器",
-                            theme: $Theme.BLUE,
-                            help: "播放器",
-                            iconSkin: ViewUtils.createIconClass(CommonConstant.Icon.EDIT),
-                            mainRegion: {
-                                comXtype: $Component.PLAYER,
-                                height: "600px",
-                                width: "400px",
-                                comConf: {
-                                    playerMode: $cons.playerMode.FLV,
-                                    url: "http://imgoss.hkz2.com/public/attachment/201709/215874/1505017975433.png"
+                                    url: ""
                                 }
                             }
                         }
