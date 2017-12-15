67f71bb9a1ef2c0e123a882b9ea04836251daf32
longhumen <huangfeng19820712@gmail.com>
2017-12-15 17:06:40 +0800

    1.修改样式的路径

--- example/index.js
+++ example/index.js
@@ -10,7 +10,7 @@ define([
         $Component.NAVIGATIONBAR.src,
         //必须先加载BaseView对象，不然后续BaseView的子类会显示不出来
         "core/js/base/BaseView",
-        "css!"+APP_NAME+"/css/app.css"
+        "css!"+APP_NAME+"/resources/styles/app.css"
     ], function ( SinglePageApplication,
                   NavigationBar) {
     var app = SinglePageApplication.extend({
